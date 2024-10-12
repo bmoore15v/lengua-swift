b229845bc27163b267bee9b0c593e4508ab9e71a
Barry Moore <barry@toworkandkeep.com>
2024-10-12 00:16:03 -0700

    formatting clean up

--- Sources/Lexer/Lexer.swift
+++ Sources/Lexer/Lexer.swift
@@ -1,103 +1,110 @@
 public class Lexer {
-    var input: String
-    var position: Int = 0
-    var readPosition: Int = 0
-    var ch: Character? = nil
+  var input: String
+  var position: Int = 0
+  var readPosition: Int = 0
+  var ch: Character? = nil
 
-    public init(input: String) {
-        self.input = input
-        readChar()
-    }
+  public init(input: String) {
+      self.input = input
+      readChar() // Advance to the next character
+  }
 
-    func readChar() {
-        if readPosition >= input.count {
-            ch = nil
+  public func nextToken() -> Token {
+    var tok: Token
+
+    skipWhitespace()
+
+    switch ch {
+      case "=":
+        tok = newToken(tokenType: .ASSIGN, ch: ch!)
+      case ";":
+        tok = newToken(tokenType: .SEMICOLON, ch: ch!)
+      case "(":
+        tok = newToken(tokenType: .LPAREN, ch: ch!)
+      case ")":
+        tok = newToken(tokenType: .RPAREN, ch: ch!)
+      case ",":
+        tok = newToken(tokenType: .COMMA, ch: ch!)
+      case "+":
+        tok = newToken(tokenType: .PLUS, ch: ch!)
+      case "{":
+        tok = newToken(tokenType: .LBRACE, ch: ch!)
+      case "}":
+        tok = newToken(tokenType: .RBRACE, ch: ch!)
+      case nil:
+        tok = Token(type: .EOF, literal: "")
+      default:
+        if isLetter(ch: ch!) {
+            let identifier = readIdentifier()
+
+            tok = Token(type: lookupIdent(identifier), literal: identifier)
+
+            return tok
+        } else if isDigit(ch: ch!) {
+            tok = Token(type: .INT, literal: readNumber())
+            
+            return tok
         } else {
-            ch = input[input.index(input.startIndex, offsetBy: readPosition)]
+            tok = newToken(tokenType: .ILLEGAL, ch: ch!)
         }
-
-        position = readPosition
-        readPosition += 1
     }
 
-    func newToken(tokenType: TokenType, ch: Character) -> Token {
-        return Token.init(type: tokenType, literal: String(ch))
+    readChar() // Advance to the next character
+    return tok
+  }
+
+  func readChar() {
+    if readPosition >= input.count {
+      ch = nil
+    } else {
+      ch = input[input.index(input.startIndex, offsetBy: readPosition)]
     }
 
-    func isLetter(ch: Character) -> Bool {
-        return ("a" <= ch && ch <= "z") || ("A" <= ch && ch <= "Z") || (ch == "_")
+    position = readPosition
+    readPosition += 1
+  }
+
+  func newToken(tokenType: TokenType, ch: Character) -> Token {
+    return Token.init(type: tokenType, literal: String(ch))
+  }
+
+  func isLetter(ch: Character) -> Bool {
+    return ("a" <= ch && ch <= "z") || ("A" <= ch && ch <= "Z") || (ch == "_")
+  }
+
+  func readIdentifier() -> String {
+    let start = position
+
+    while isLetter(ch: ch!) {
+      readChar() // Advance to the next character
     }
 
-    public func readIdentifier() -> String {
-        let start = position
-        while let char = ch, isLetter(ch: char) {
-            readChar()
-        }
-        let startIndex = input.index(input.startIndex, offsetBy: start)
-        let endIndex = input.index(input.startIndex, offsetBy: position)
-        return String(input[startIndex..<endIndex])
-    }
+    let startIndex = input.index(input.startIndex, offsetBy: start)
+    let endIndex = input.index(input.startIndex, offsetBy: position)
 
-    func skipWhitespace() {
+    return String(input[startIndex..<endIndex])
+  }
+
+  func skipWhitespace() {
     // While the current character is a space, tab, newline, or carriage return, keep reading the next character
-    while let currentChar = ch, currentChar == " " || currentChar == "\t" || currentChar == "\n" || currentChar == "\r" {
-        readChar() // Advance to the next character
+    while ch == " " || ch == "\t" || ch == "\n" || ch == "\r" {
+      readChar() // Advance to the next character
     }
-}
+  }
 
-    func isDigit(ch: Character) -> Bool {
-        return "0" <= ch && ch <= "9"
-    }
+  func isDigit(ch: Character) -> Bool {
+    return "0" <= ch && ch <= "9"
+  }
 
-    func readNumber() -> String {
-        let start = position
-        while let currentChar = ch, isDigit(ch: currentChar) {
-            readChar()
-        }
-        let startIndex = input.index(input.startIndex, offsetBy: start)
-        let endIndex = input.index(input.startIndex, offsetBy: position)
-        return String(input[startIndex..<endIndex])
+  func readNumber() -> String {
+    let start = position
+
+    while isDigit(ch: ch!) {
+      readChar()  // Advance to the next character
     }
 
-
-    public func nextToken() -> Token {
-        var tok: Token
-
-        skipWhitespace()
-
-        switch ch {
-            case "=":
-                tok = newToken(tokenType: .ASSIGN, ch: ch!)
-            case ";":
-                tok = newToken(tokenType: .SEMICOLON, ch: ch!)
-            case "(":
-                tok = newToken(tokenType: .LPAREN, ch: ch!)
-            case ")":
-                tok = newToken(tokenType: .RPAREN, ch: ch!)
-            case ",":
-                tok = newToken(tokenType: .COMMA, ch: ch!)
-            case "+":
-                tok = newToken(tokenType: .PLUS, ch: ch!)
-            case "{":
-                tok = newToken(tokenType: .LBRACE, ch: ch!)
-            case "}":
-                tok = newToken(tokenType: .RBRACE, ch: ch!)
-            case nil:
-                tok = Token(type: .EOF, literal: "")
-            default:
-                if isLetter(ch: ch ?? "2") {
-                    let identifier = readIdentifier()
-                    tok = Token(type: lookupIdent(identifier), literal: identifier)
-                    return tok
-                } else if isDigit(ch: ch ?? "3") {
-                    tok = Token(type: .INT, literal: readNumber())
-                    return tok
-                } else {
-                    tok = newToken(tokenType: .ILLEGAL, ch: ch!)
-                }
-        }
-
-        readChar()
-        return tok
-    }
+    let startIndex = input.index(input.startIndex, offsetBy: start)
+    let endIndex = input.index(input.startIndex, offsetBy: position)
+    return String(input[startIndex..<endIndex])
+  }
 }
